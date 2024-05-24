79a6a3753b8242bbb3b27f8711a7b4f51014d669
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2024-05-24 20:13:33 +0530

    Fronend - login/signup page

--- frontend/react-ecommerce/src/App.js
+++ frontend/react-ecommerce/src/App.js
@@ -4,12 +4,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </>
   );
 }
 
